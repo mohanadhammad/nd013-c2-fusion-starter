cbf89062744b41d0bf223c44bdd2dbf725ef5c8e
Mohanad Youssef <mohanad.youssef@continental-corporation.com>
2021-11-14 17:25:28 +0100

    first task in project 1

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -29,6 +29,7 @@ from tools.waymo_reader.simple_waymo_open_dataset_reader import dataset_pb2, lab
 # object detection tools and helper functions
 import misc.objdet_tools as tools
 
+import zlib
 
 # visualize lidar point-cloud
 def show_pcl(pcl):
@@ -59,18 +60,39 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get laser data structure from frame
+    ri = []
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first sequence
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
     
     # step 2 : extract the range and the intensity channel from the range image
+    ri_range_ch = ri[:, :, 0]
+    ri_inten_ch = ri[:, :, 1]
     
     # step 3 : set values <0 to zero
+    ri_range_ch[ri_range_ch < 0.0] = 0.0
+    ri_inten_ch[ri_inten_ch < 0.0] = 0.0
     
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    ri_range_ch = ri_range_ch * 255 / (np.amax(ri_range_ch) - np.amin(ri_range_ch))
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
+    ri_inten_ch = np.amax(ri_inten_ch)/2 * ri_inten_ch * 255 / (np.amax(ri_inten_ch) - np.amin(ri_inten_ch))
     
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    
-    img_range_intensity = [] # remove after implementing all steps
+    #img_range_intensity = [] # remove after implementing all steps
+    ri_range_ch = ri_range_ch.astype(np.uint8)
+    ri_inten_ch = ri_inten_ch.astype(np.uint8)
+
+    deg45 = int(ri_range_ch.shape[1] / 8)
+    center = int(ri_range_ch.shape[1] / 2)
+
+    ri_range_ch = ri_range_ch[:, center-deg45 : center+deg45]
+    ri_inten_ch = ri_inten_ch[:, center-deg45 : center+deg45]
+
+    img_range_intensity = np.vstack((ri_range_ch, ri_inten_ch))
     #######
     ####### ID_S1_EX1 END #######     
     
